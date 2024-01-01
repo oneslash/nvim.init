08f46786bf25ba2ee719244f6386f66fbd1b88c6
Sardorbek Pulatov <sardo@hey.com>
2024-01-01 22:32:18 +0100

    cleanup

--- init.lua
+++ init.lua
@@ -95,7 +95,9 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = { enabled = false },
+    },
   },
 
   -- "gc" to comment visual regions/lines
---
+++ lua/custom/plugins/onedark.lua
@@ -0,0 +1,2 @@
+return { 'navarasu/onedark.nvim', }
+
